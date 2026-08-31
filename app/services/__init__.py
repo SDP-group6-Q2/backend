afd5010b23e1cd42517bcf690e52d84b9781ba63
Alain Rizzi <s350714@studenti.polito.it>
2026-08-31 16:09:53 +0300

    added db related conversation and message table and also upgraded existing services

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,5 +1,15 @@
 from app.services.client_service import ClientService, get_client_service
 from app.services.user_manager import UserManager, get_user_manager
 from app.services.assistant_service import AssistantService, get_assistant_service
+from app.services.chat_service import ChatService, get_chat_service
 
-__all__ = ["ClientService", "get_client_service", "UserManager", "get_user_manager", "AssistantService", "get_assistant_service"]
+__all__ = [
+    "ClientService",
+    "get_client_service",
+    "UserManager",
+    "get_user_manager",
+    "AssistantService",
+    "get_assistant_service",
+    "ChatService",
+    "get_chat_service",
+]
