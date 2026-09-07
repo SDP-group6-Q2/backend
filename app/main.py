2bf90123d00515c773113f246f127266f13478ad
Alain Rizzi <s350714@studenti.polito.it>
2026-09-07 13:36:18 +0300

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.auth import auth_backend, fastapi_users
 from app.routes.users import router as users_router
@@ -6,6 +7,14 @@ from app.routes.clients import router as clients_router
 from app.routes.assistant import router as assistant_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
